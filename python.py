8f74fdb1702ba7a0b7a85c86c55e17e26f51970e
Mariana Parra <mariana.parra@iprojectpartners.com.bo>
2022-10-05 16:14:33 -0400

    version2

---
+++ python.py
@@ -0,0 +1,15 @@
+import cx_Oracle
+from sqlalchemy.engine import create_engine
+import pandas as pd 
+
+DIALECT = 'oracle'
+SQL_DRIVER = 'cx_oracle'
+USERNAME = 'parram' 
+PASSWORD = 'Ippb202209'
+HOST = 'DESKTOP-D9FCLPV' 
+PORT = 1521 
+SERVICE = 'BISSAS' 
+ENGINE_PATH_WIN_AUTH = DIALECT + '+' + SQL_DRIVER + '://' + USERNAME + ':' + PASSWORD +'@' + HOST + ':' + str(PORT) + '/?service_name=' + SERVICE
+# cx_Oracle.init_oracle_client(lib_dir=r"C:\python\pruebas\oracle\instantclient_19_11")
+engine = create_engine(ENGINE_PATH_W
+===============================
